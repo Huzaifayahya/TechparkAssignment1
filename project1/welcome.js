36dea839e39fc4a378c613153c6883174116dd37
Ahmedaftab12 <ahmedsheikh1390@gmail.com>
2025-02-10 10:05:11 +0500

    javascript

--- project1/welcome.js
+++ project1/welcome.js
@@ -1,3 +1,4 @@
+const Welcome=alert("Welcome to Merath kabab House");
 const day =prompt ("Enter your day");
 // if (day != "monday" || "tuesday" || "wednesday" || "thursday" || "friday" || "saturday" || "sunday")
 //     {console.log("invalid day");} 
@@ -5,30 +6,33 @@ const budget =prompt("Enter your budget");
 if (budget < 1){
     console.log("invalid budget");
 }else{
-    if(day == "monday"){
+    if(day == "monday" || day== "tuesday" || day== "wednesday" || day== "thursday"){
         if  (budget > 1000){
             console.log("Deal 1 && Deal 2" );
         }else if(budget >= 500){
+            alert("Deal 1");
             console.log("Deal 1");
         }else if(budget < 500){
+            alert("Deal 1");
             console.log("Deal 2");
         }else{
+            alert("No deals available")
             console.log("No deals available");
         }
     }else if (day == "friday"){
         if(budget == 1000){
+            alert("Buffet")
             console.log("Buffet");
         }else{
+            alert("No deals available")
             console.log("No deals available");
         }
     }else if (day == "sunday"){
-        if(budget >= 1500){
-            let desert = confirm("Want a desert?");
-            if(desert){
-                console.log("Deal and Desert");
-            } else {
-            console.log("Deal");
-        }
+        var Desert =prompt("Do you want Desert?")
+        if(budget >= 1500 && Desert == 'yes'){
+                console.log("Mega Deal and Desert");
+            } else if(budget >= 1500) {
+            console.log("Mega Deal");
     }else{
         console.log("No deals available");
     }
