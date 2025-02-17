6ec0bf5520ff42cec6d7ee0ffdbc9bfc2789b9a5
Huzaifa <huzaifayahya2007@gmail.com>
2025-02-17 12:13:56 +0500

    new exercise

---
+++ project1/Exercise3.js
@@ -0,0 +1,152 @@
+// let a=5;
+// let b=6;
+
+// a = a + b;
+// b = a - b;
+// a = a - b;
+
+// console.log(a)
+// console.log(b)"
+// const loginIn = "";
+
+// const proSubs = "";
+
+
+// if (loginIn != true) {
+//     console.log("PLease, first login to see the Data");
+// }
+// else if (proSubs != true) {
+//     console.log("You're logged in  but not a subscriber!");
+// }
+// else if (proSubs == true) {
+//     console.log("Thanks, you're a pro subscriber");
+// }
+
+// console.log("You're Done!");
+
+
+
+
+
+// const grade = prompt("Enter You're Percentage");
+
+// if (grade > 100 || grade < 0) {
+//     console.log("Invalid value!");
+// } else if (grade >= 90) {
+//     console.log("You're percentage is " + grade + " & you're grade is A");
+// }else if (grade >= 80) {
+//     console.log("You're percentage is " + grade + " & you're grade is B");
+// }else if (grade >= 70) {
+//     console.log("You're percentage is " + grade + " & you're grade is C");
+// }else if (grade >= 60) {
+//     console.log("You're percentage is " + grade + " & you're grade is D");
+// }else{
+//     console.log("You're percentage is " + grade + " & you're grade is F");
+// }
+
+
+
+
+// // menu 
+// // const day = prompt("Enter you're ");
+
+// const daysOfWeek = ["monday", "tuesday", "wednesday", "thursday", "friday", "saturday", "sunday"];
+// let currentDay = prompt("Enter day of week:");
+// while (!daysOfWeek.includes(currentDay)) {
+//   currentDay = prompt("Invalid day. Enter day of week:").toLowerCase();
+// }
+
+
+
+
+// let a = 4;
+// let b = 7;
+// console.log("a", a);
+// console.log("b", b);
+// [a,b] = [b,a];
+// console.log("a", a);
+// console.log("b", b);
+
+
+// let numbers = [1,2,4,6,2,1,6,9,8,0,0,2,3,4,5,6,7];
+// let unique = [];
+
+// for(let i = 0; i < arr.length; i++){
+//     const element = arr[i];
+//     if (unique.includes[element] === false) {
+//         unique.push[element];
+//     }
+// }
+
+// console.log('number',numbers);
+// console.log('unique', unique);
+
+
+// const products = {
+//     name: 'HP Elitebook sleeve',
+//     instock: true,
+//     price: 1000,
+//     totalUnits: 7,
+//     color: ["black", "white", "gray"]
+// };
+
+// console.log(products.color[2]);
+
+
+// const obj = {
+//     product1: [
+
+//     ],
+// };
+
+
+
+
+// const resturant = {
+//     name: 'Ichiran Binner',
+//     address: `${Math.floor(Math.random() * 100) + 1} Abu Hammad`,
+//     city: 'Karachi',
+//     state: 'PK',
+//     zipcode: '70165'
+// };
+// let fulAdrress = `${resturant['address']}, ${resturant['city']}, ${resturant['state']}, ${resturant['zipcode']}`;
+// console.log(fulAdrress);
+
+
+
+
+
+
+
+// let person = {
+//     name: 'Abu Hammad',
+//     age: 21,
+//     city: 'Karachi'
+// };
+// person.age = 22
+// person.city = 'UK'
+// person['job'] = 'Backend Dveloper'
+// console.log(person);
+// console.log(person.age);
+// delete person.age;
+// console.log(person);
+
+
+
+
+
+
+
+
+// let student = {
+//     name: 'Abu Hammad',
+//     age: '22',
+//     subjetcs: ['Match', 'English', 'Urdu'],
+//     isEnrolled: true
+// };
+// student.grade = 'B';
+// console.log('Name' + ': ' + student['name']);
+// console.log('Age' + ': ' + student['age']);
+// console.log('subject' + ': ' + student.subjetcs);
+// console.log('isEnrolled' + ': ' + false);
+// console.log('Grade' + ': ' + student.grade);
