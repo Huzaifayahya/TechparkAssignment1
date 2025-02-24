22b854bf490412461794fd6a77d4b22efc174d45
Huzaifa <huzaifayahya2007@gmail.com>
2025-02-24 10:24:41 +0500

    new file added

---
+++ project1/Exercise4.js
@@ -0,0 +1,15 @@
+/ const arr = ["A","B", "C","a","b", {a: 2}];
+// console.log(arr.indexOf({a: 2}));
+
+// const nameReverse = "we are learning javascript"
+// for (let i=0; i < nameReverse.length -1; i++) {
+    // nameReverse.length = +
+// }
+
+
+function add(a, b) {
+    return a + b;
+}
+
+let result = add(5, 5);
+console.log(result);
